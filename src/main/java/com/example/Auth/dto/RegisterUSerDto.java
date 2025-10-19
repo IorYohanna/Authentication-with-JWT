44b627832ef6e035acf9484f9c13daf99771d06a
IorYohanna <yorakotosandratana@gmail.com>
2025-10-19 19:14:10 +0300

    created email config and added dto files for test

---
+++ src/main/java/com/example/Auth/dto/RegisterUSerDto.java
@@ -0,0 +1,12 @@
+package com.example.Auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUSerDto {
+    private String username;
+    private String email;
+    private String password;
+}
